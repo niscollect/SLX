98a537ed9023e671821adb3fc6170ea42610aaf9
niscollect <collectnis@gmail.com>
2025-12-23 20:48:34 +0530

    deleted an unnecessary file

--- test.js
+++
@@ -1,8 +0,0 @@
-import { resoResolve } from "./src/core/resolve.js";
-import { validPathsList } from "./src/core/paths.js";
-
-const validPaths = validPathsList();
-
-const url = resoResolve('/bad-luck.html', validPaths);
-
-console.log(url);
