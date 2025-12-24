5ec92dce1a4035779ec873c9ef3d07b0f17350e7
niscollect <collectnis@gmail.com>
2025-12-25 01:13:37 +0530

    nothing much

--- src/core/crawl.js
+++ src/core/crawl.js
@@ -54,50 +54,18 @@ export async function crawl(rootDir)
 
     console.log(setOfAllFiles);
 
+    // ----------------------------------------------------------------------
     // synchronous VERY VERY BAD CHOICE
     // sequential async  Simple/safer that the above   NOT A GOOD CHOICE
     // parallel async  NOT A GOOD CHOICE
     // bounded parallelism (BATCHED) batches of files  REALLY GOOD CHOICE
+    // ----------------------------------------------------------------------
+
+    // for easier way to batch, we can just have them as an array, that's the easiest way of having them indexed
+    // arrayOfAllFiles = [...setOfAllFiles];  OR
+    const arrayOfAllFiles = Array.from(setOfAllFiles);
 
     
 
-    // try 
-    // {
-        // let files = await readdir(rootDir);
-
-
-        // console.log("==========\n", files, "\n========");
-
-        // for(const item of files) // used for-of and not for-each, coz for-of is async aware and for-each isn't
-        // {
-            // if(item == "node_modules" || item == ".env" || item == ".gitignore")
-            //     continue;
-
-        //     try
-        //     {
-        //         let tempPath = rootDir + "\\" + item;
-        //         let subdirs = await readdir(tempPath);
-
-        //         console.log(`subdirs of ${tempPath} are:`, subdirs);
-        //     }
-        //     catch(error)
-        //     {
-        //         if(error.code == "ENOTDIR")
-        //         {
-        //             console.error(`${item} is a file, not a folder`);
-        //         }
-        //         else
-        //             throw error;
-        //     }
-
-            
-        // }
-
-    // }
-    // catch (error) 
-    // {
-    //     console.error("Error in accessing files: ", error);
-    // }
-    
 
 }
