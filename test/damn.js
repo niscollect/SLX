27e45f7eeacdd3b46123c1245416b7fdd1009bac
niscollect <collectnis@gmail.com>
2026-01-01 17:20:06 +0530

    test feat: implement static AST scan for Express routes

--- test/damn.js
+++ test/damn.js
@@ -1,62 +1,118 @@
+//?---------------- A basic understanding of terms ---------------------
+//* MemberExpression accesses a property or method of an object
+//* CallExpression executes a function or method
+
+//?---------------------------------------------------------------------
+
 import { readFile } from "fs/promises";
 import { parse } from "acorn";
-
 import * as walk from "acorn-walk";
-
 
 const filePath = "./index.js"
 
 // our data object that we'll export
-// const data = [];
+const routes = [];
 
-async function walker(node)
+function extractMethodName(callee)
 {
     
-    walk.recursive(node, {
-        // Literal(node)
-        // {
-        //     console.log("Found a literal:", node.value);
-        // }
-        //? searching Literals is good, but isn't prudent enough, coz they can even use Identifiers; and we can't keeping looking for both, as they'll then be more than imagination
 
-        //? instead we search for CallExpressions
-        CallExpression(node, state, c)
+
+}
+
+function extractFileFromHandler(handlerBody) 
+{
+    const RESPONSE_METHODS = ["send", "sendFile", "render", "json"];
+    const FILE_EXTENSIONS = [".html", ".ejs", ".xml", ".pug", ".hbs", ".jade"]; // possibly the presentable files
+
+    let extractedFile = null;
+
+    walk.simple(handlerBody, {
+        
+        CallExpression(callNode)
         {
-            // inside CallExpression, endpoint can be either an indentifier or a literal
-            // currently we'll keep the restriction to look for literals only
-            
-            // walk.simple(node, {
-            //     Literal(node)
-            //     {
-            //         console.log("==", node.value);
-            //     }
-            // });
+            // Extract method name from callee (handles nested cases)
+            let methodName = extractMethodName(callNode.callee);
 
+            // if(callNode.callee.type === "MemberExpression")
+            //     methodName = callNode.callee.property.name;
+            // covered this case in the extractMethodName function
 
-            console.log("Found CallExpression, searching its children for Literals...");
-            // walk.recursive(node,{},{
-            //     Literal(node)
-            //     {
-            //         console.log("==", node.value);
-            //     },
-            //     Expression(node, state, c) {
-            //         c(node, state);
-            //     }
-            // })
-            walk.simple(node, {
-                Literal(literalNode) {
-                    console.log("== Literal found inside Call:", literalNode.value);
+            // Just Skip if method name doesn't match eligible ones, or is empty
+            if(!methodName || !RESPONSE_METHODS.includes(methodName))
+                return;
+
+            // Now, look for Literals in the arguments
+            callNode.arguments.forEach(arg => {
+
+                if(arg.type === "Literal" && typeof(arg.value) === "string")
+                { // may be we are close
+                    console.log("here", arg.value);
+
+                    // Remove surrounding quotes if they exist (both ' and ")
+                    // let cleanName = arg.value.replace(/['"]+/g, '');   
+                    let cleanName = arg.value.replace(/['"]+/g, '').split(/[?#]/)[0].trim();
+                    //check if the literals end with any eligible extensions, i.e. is an eligible presentable file
+                    if(FILE_EXTENSIONS.some(ext => cleanName.endsWith(ext)))
+                    {
+                        console.log("arg value log", arg.value);
+                        extractedFile = arg.value;
+                    } 
                 }
             });
-        },     
-        Expression(node, state, c) {
-            c(node, state);
-        },
-        Statement(node, state, c) {
-            c(node, state);
-        },
-        Program(node, state, c) {
-            c(node, state);
+        }
+    });
+
+    console.log(extractedFile, "---");
+    return extractedFile;
+}
+
+
+function walker(node)
+{
+    const HTTP_VERBS = ["get", "post", "put", "delete", "use"];
+    
+    walk.simple(node, {
+        CallExpression(callNode)
+        {    
+            // Step 1: Filter only CallExpressions with MemberExpression callee
+            if (callNode.callee.type !== "MemberExpression") return;
+
+            // Step 2: Check if property name is an HTTP verb
+            const methodName = callNode.callee.property.name;
+            if (!HTTP_VERBS.includes(methodName)) return;
+
+            // Step 3: Reject if less than 2 arguments (need path + handler minimum), for it to be an express route
+            if (callNode.arguments.length < 2) return;
+            
+            // Step 4: Extract literals and arrow functions
+            const path = callNode.arguments[0];
+            const handler = callNode.arguments[1];
+
+            // Only process if path is a Literal 
+            if (path.type !== "Literal") return;  //! only literals for now. We'll build for betterment and more cases, later on
+
+            // Only process if handler is an ArrowFunctionExpression
+            if (handler.type !== "ArrowFunctionExpression") return;  //! only arrowFunctions for now. We'll build for betterment and more cases, later on
+
+
+            //* Branch-1: Walking arrow function body
+            // Step 5: Extract file from handler body
+            const file = extractFileFromHandler(handler.body);
+            console.log("\n\t\t", file, "\t\t", typeof(file), "\n")
+
+
+
+
+            // If we reach here, it's possibly a valid route
+            routes.push({
+                method: methodName,
+                path: path.value,
+                file: file || null
+            });
+
+            console.log(`✓ Found route: ${methodName.toUpperCase()} ${path.value}`);
+
         }
     })
 }
@@ -75,8 +131,18 @@ async function parseFile()
 
     //@note : We are only looking for express routes
 
-    await walker(ast);
+    // await walker(ast);
+    walker(ast);
 
+    console.log("\n=== Extracted Routes ===");
+
+    // console.log(JSON.stringify(, null, 2));
+
+    let Routesobj = routes.map(r => ({method: r.method, path: r.path, file: r.file }))
+
+    const jsonString = JSON.stringify(Routesobj, null, 2);
+
+    console.log(jsonString);
 }
 
 await parseFile();
